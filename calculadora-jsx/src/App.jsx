9b8663d782a90d536c6bc817efb7e12bb793a957
Gekyume <contatogekyume@gmail.com>
2024-06-19 17:51:45 -0300

    start-with-components

---
+++ calculadora-jsx/src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/App.css';
+import React from "react";
+import {Calculadora} from "./components/Calcualdora/Calculadora";
+
+function App() {
+  return (
+    <>
+      <Calculadora />
+    </>
+  );
+}
+
+export default App;
+ 
