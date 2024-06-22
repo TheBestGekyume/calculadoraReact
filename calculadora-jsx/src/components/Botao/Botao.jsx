2391f44570667f5e8f6c8dc508fbf8030741c774
Gekyume <contatogekyume@gmail.com>
2024-06-21 21:44:38 -0300

    Botões(INCOMPLETO)

---
+++ calculadora-jsx/src/components/Botao/Botao.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Botao.css';
+
+export function Teclado(props) {
+    return (
+        <div id='teclado'>
+            <button className='botao'></button>
+        </div>
+    );
+}
---
+++ calculadora-jsx/src/components/Teclado/Teclado.jsx
@@ -0,0 +1,23 @@
+import './Teclado.css';
+
+export function Teclado() {
+    return (
+        <div id='teclado'>
+            <button className='botao'>9</button>
+            <button className='botao'>8</button>
+            <button className='botao'>7</button>
+            <button className='botao'>6</button>
+            <button className='botao'>5</button>
+            <button className='botao'>4</button>
+            <button className='botao'>3</button>
+            <button className='botao'>2</button>
+            <button className='botao'>1</button>
+            <button className='botao'>0</button>
+            <button className='botao'>,</button>
+            <button className='botao'>&#8678;</button>
+            <button className='botao'>&#8678;</button>
+            <button className='botao'>&#8678;</button>
+            <button className='botao'>&#8678;</button>
+        </div>
+    );
+}
